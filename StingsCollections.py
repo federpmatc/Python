2ba10427a2f6823876174822e439ac811d3c5223
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-01-20 16:15:54 -0600

    Create StingsCollections.py

---
+++ StingsCollections.py
@@ -0,0 +1,17 @@
+#str, bytes, list, and dict
+#collection types
+#String Literals
+print('Hello')
+print('"Hi" she said')
+
+#python auto concatenates strings
+print("hello" 'how are' "you")
+
+print("""this is a 
+multiline 
+string""")
+
+print("Hello \n mulitline \n string")
+#escape sequences
+print("Hello \" \\ !")
+#stopped at #4 Strings
